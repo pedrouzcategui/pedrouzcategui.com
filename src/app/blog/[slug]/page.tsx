9f59b669ccfcd2e9f15665d65e9a67bdf9911fb1
Pedro <pedrouzcategui0103@gmail.com>
2024-01-15 23:17:14 -0400

    Adds Prisma!!!!!!!!!!!

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,55 +1,15 @@
-import getFormattedDate from "@/lib/getFormattedDate"
-import { getSortedPostsData, getPostData } from "@/lib/posts"
-import { notFound } from "next/navigation"
 import Link from "next/link"
 import styles from "./style.module.css"
-import hero from "@/public/assets/grid-pattern.png"
-
-export function generateStaticParams() {
-    const posts = getSortedPostsData()
-
-    return posts.map((post) => ({
-        postSlug: post.slug
-    }))
-}
-
-export function generateMetadata({ params }: { params: { slug: string } }) {
-
-    const posts = getSortedPostsData()
-    const { slug } = params
-
-    const post = posts.find(post => post.slug === slug)
-
-    if (!post) {
-        return {
-            title: 'Post Not Found'
-        }
-    }
-
-    return {
-        title: post.title + " - Pedro Uzcategui",
-    }
-}
 
 export default async function Post({ params }: { params: { slug: string } }) {
-
-    const posts = getSortedPostsData()
-    const { slug } = params
-
-    if (!posts.find(post => post.slug === slug)) notFound()
-
-    const { title, date, contentHtml } = await getPostData(slug)
-
-    const pubDate = getFormattedDate(date)
-
     return (
         <main className="min-h-screen bg-gradient-to-r from-black via-slate-950 to-gray-950 text-white">
             <div className="w-2/4 mx-auto p-5">
                 <div>
                     <Link href="/blog">← Back to blog posts</Link>
-                    <h1 className="text-4xl mt-5 mb-5  font-bold">{title}</h1>
+                    <h1 className="text-4xl mt-5 mb-5  font-bold">Title</h1>
                     <p className="mt-0">
-                        {pubDate}
+                        11
                     </p>
                     <p>Author: Pedro Uzcategui</p>
                     <p>Follow me on Twitter: <a target="_blank" href="https://twitter.com/PedroUzctegui4">Here</a></p>
@@ -59,7 +19,6 @@ export default async function Post({ params }: { params: { slug: string } }) {
                 </div>
                 <div>
                     <article className={`${styles.article}`}>
-                        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
                     </article>
                 </div>
             </div>
