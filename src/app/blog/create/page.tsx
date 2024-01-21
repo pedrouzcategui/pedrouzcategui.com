1e4ea2094388a653c4897743fcb15763b140ab94
Pedro <pedrouzcategui0103@gmail.com>
2024-01-21 15:14:27 -0400

    Add Todo for certain pages

---
+++ src/app/blog/create/page.tsx
@@ -0,0 +1,1 @@
+// Todo: WYSIWYG Editor!
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,20 +1,16 @@
 import Link from "next/link";
 import styles from "./styles.module.css";
-import { getSortedPostsData } from "../../../lib/posts";
 export const metadata = {
     title: 'Blog',
     description: 'Blog',
 };
 
 export default function NamePage() {
-    const posts = getSortedPostsData();
     return (
         <div className="p-5 min-h-screen bg-gradient-to-r from-black via-slate-950 to-gray-950 text-white">
             <h1 className="text-2xl font-bold">Blog posts</h1>
             <ul className={styles.list}>
-                {posts.map(post => {
-                    return <li key={post.slug}><Link href={`/blog/${post.slug}`}>{post.title}</Link></li>
-                })}
+            //TODO: Blogs
             </ul>
         </div>
     );
