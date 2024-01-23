58ce2f100c7fb142744fa61d41faa3b591fc2e67
Pedro <pedrouzcategui0103@gmail.com>
2024-01-22 22:07:35 -0400

    Add Blog

--- src/app/blog/create/page.tsx
+++ src/app/blog/create/page.tsx
@@ -1,1 +1,7 @@
-// Todo: WYSIWYG Editor!
+// https://medium.com/@MatDrinksTea/rendering-markdown-and-latex-in-react-dec355e74119
+export default function BlogCreatePage() {
+    return (
+        <div>
+        </div>
+    );
+}
